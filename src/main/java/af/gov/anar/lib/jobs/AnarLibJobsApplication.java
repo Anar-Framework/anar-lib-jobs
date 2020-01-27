24555da55be4ecd382e739954a1e9cebc55292f0
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-27 09:59:23 +0430

    jobs library of anar framework

---
+++ src/main/java/af/gov/anar/lib/jobs/AnarLibJobsApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.lib.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarLibJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarLibJobsApplication.class, args);
+	}
+
+}
